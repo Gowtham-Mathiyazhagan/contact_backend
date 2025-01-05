b7638ccaef0e9dae25fbaacd3c894b0823436a93
Gowtham-Mathiyazhagan <gowthammathi07@gmail.com>
2025-01-05 22:07:17 +0530

    initial commit with boilerplate

---
+++ index.js
@@ -0,0 +1,18 @@
+const { connectDb } = require("./config/connectDB");
+const { errorHandler } = require("./middlewrare/errorHandler");
+const express = require("express");
+
+const dontenv = require("dotenv").config();
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+connectDb();
+
+app.use(express.json());
+app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use(errorHandler); // cretaed custom middleware to handle the error
+app.listen(port, () => {
+  console.log("PORT Listen on " + port);
+});
